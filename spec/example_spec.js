200abfe2d2b0a0bc6abaabb67f92a8c36fca9c4e
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-08-20 16:07:00 +0100

    adding exports

--- spec/example_spec.js
+++ spec/example_spec.js
@@ -14,13 +14,38 @@ class TestSuite {
         this.subject = new ExampleClass;
     }
 
+    errorFunction() {
+        throw new Error ("Test error");
+    }
+
+    noErrorFunction() {
+        return 0;
+    }
+
     allTests() {
 
-        this.suite.example(Core.isEqualTo(1, 1));
-        this.suite.example(Core.isEqualTo(1, 2));
+        // persistence checks : subj should have same var value at the end of file
+        example(isEqualTo(this.subject.isOne, 1));
+        
+        example(isEqualTo(this.subject.isOne, 2));
+        example(isGreaterThan(this.subject.isOne, 2));
+        example(isGreaterThan(2, this.subject.isOne));
+        example(isEven(1));
+        example(isEven(2));
+        example(isEmpty([]));
+        example(isEmpty([1, 2, 3]));
+        example(isNumber(12));
+        example(isInteger(12));
+        example(isNumber("hekli"));
+        example(isNumber("11"));
+        example(isInteger("11"));
+        example(isNull(null));
+        example(isNull(11));
+        example(errorThrown("Test error", this.errorFunction));
+        example(errorThrown("Test error", this.noErrorFunction));
+        //example(respondsTo())
 
-
-        this.suite.writeOutput();
+        writeOutput();
 
     };
 }
