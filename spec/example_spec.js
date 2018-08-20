26ff9dd5870560462226c32783b37b32d8d2d608
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-08-20 17:21:52 +0100

    writing init script

--- spec/example_spec.js
+++ spec/example_spec.js
@@ -26,7 +26,7 @@ class TestSuite {
 
         // persistence checks : subj should have same var value at the end of file
         example(isEqualTo(this.subject.isOne, 1));
-        
+        this.subject.isOne = 2;
         example(isEqualTo(this.subject.isOne, 2));
         example(isGreaterThan(this.subject.isOne, 2));
         example(isGreaterThan(2, this.subject.isOne));
@@ -45,6 +45,9 @@ class TestSuite {
         example(errorThrown("Test error", this.noErrorFunction));
         //example(respondsTo())
 
+
+        // persistance test
+        example(isEqualTo(this.subject.isOne, 1));
         writeOutput();
 
     };
