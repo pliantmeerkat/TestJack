4814f9cc7ce2bdb46322c9a1dd1ea73dc1e473c6
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-08-21 17:07:03 +0100

    framework working, display good

--- spec/example_spec.js
+++ spec/example_spec.js
@@ -1,48 +1,15 @@
+describe("Example 1", () => {
 
-class TestSuite {
+	var subject = new ExampleClass();
 
-	constructor() {
-		this.suite = new Suite();
-		this.subject = new ExampleClass;
-	}
-
-	errorFunction() {
-		throw new Error ("Test error");
-	}
-
-	noErrorFunction() {
-		return 0;
-	}
-
-	allTests() {
-
-		// persistence checks : subj should have same var value at the end of file
-		example(isEqualTo(this.subject.isOne, 1));
-		this.subject.isOne = 2;
-		example(isEqualTo(this.subject.isOne, 2));
-		example(isGreaterThan(this.subject.isOne, 2));
-		example(isGreaterThan(2, this.subject.isOne));
-		example(isEven(1));
-		example(isEven(2));
-		example(isEmpty([]));
-		example(isEmpty([1, 2, 3]));
-		example(isNumber(12));
-		example(isInteger(12));
-		example(isNumber("hekli"));
-		example(isNumber("11"));
-		example(isInteger("11"));
-		example(isNull(null));
-		example(isNull(11));
-		example(errorThrown("Test error", this.errorFunction));
-		example(errorThrown("Test error", this.noErrorFunction));
-		example(isClass(11, String));
-		example(isClass(this.subject, ExampleClass));
-		//example(respondsTo())
-
-
-		// persistance test
-		example(isEqualTo(this.subject.isOne, 1));
-		writeOutput();
-
-	}
-}
+	it("passes a test", () => {
+		example(isEven(subject.isEvenNumber));
+	});
+	it("fails a test", () => {
+		example(isOdd(subject.isEvenNumber));
+	});
+	it("is true", () => {
+		example(isTrue(false));
+	});
+});
+	
--- src/example.js
+++ src/example.js
@@ -1,14 +1,14 @@
 class ExampleClass {
-    constructor() {
-        this.isTrue = true;
-        this.isFalse = false;
-        this.isOne = 1;
-        this.isTen = 10;
-        this.isEvenNumber = 2;
-        this.isOddNumber = 3;
-        this.isPrimeNumber = 5;
-        this.isAString = "Im A Sring";
-        this.isAnEmptyArray = [];
-        this.isAItemedArray = [1, 2, 3];
-    };
+	constructor() {
+		this.isTrue = true;
+		this.isFalse = false;
+		this.isOne = 1;
+		this.isTen = 10;
+		this.isEvenNumber = 2;
+		this.isOddNumber = 3;
+		this.isPrimeNumber = 5;
+		this.isAString = "Im A Sring";
+		this.isAnEmptyArray = [];
+		this.isAItemedArray = [1, 2, 3];
+	}
 }
